c7dc431cee4ee77585cd2f6f88a48bef781082dc
Anees <aneesnafeesimmu@gmail.com>
2024-08-21 15:34:29 +0530

    first-commit

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+const Body = () => {
+    return(
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="backdrop-blur-lg bg-white/20 w-2/3 p-72 rounded-3xl">
+                <h1>Hi Anees!</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
