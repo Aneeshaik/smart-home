bc6be4eb3ad09d2eb2287f718eb223dedfbbc059
Anees <aneesnafeesimmu@gmail.com>
2024-08-22 20:07:07 +0530

    openAPI-issue

--- src/components/Right.js
+++ src/components/Right.js
@@ -4,6 +4,7 @@ const Right = () => {
     const [time, setTime] = useState(new Date().toLocaleTimeString('en-us', {hour12: false}));
     const date = new Date().toLocaleDateString('en-us', {month: 'long', day: '2-digit'});
     const day = new Date().toLocaleDateString('en-us', {weekday: 'long'})
+    const [weatherData, setWeatherData] = useState({});
     const [hours, minutes] = time.split(':')
     useEffect(() => {
             const timer = setInterval(() => {
@@ -11,12 +12,32 @@ const Right = () => {
             }, 1000);
             return () => clearInterval(timer); // Cleanup the interval on component unmount
         }, []);
+    useEffect(() => {
+        fetchData();
+    },[])
+    
+    const fetchData = async () => {
+        const apiKey = process.env.REACT_APP_YOUR_API_KEY;
+        console.log(apiKey); // Ensure this logs the correct API key
+    
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`);
+            const weatherData = await response.json();
+            setWeatherData(weatherData);
+            console.log(weatherData);
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
+    };
+    
+    
 
     return (
         <div className="backdrop-blur-3xl m-2 bg-white/20 rounded-3xl text-white">
             <div className="p-3">
                 <h1 className="font-semibold text-2xl">{hours} : {minutes}</h1>
                 <h1>{day}, {date}</h1>
+                <h1>{weatherData.main.temp}</h1>
             </div>
         </div>
     )
