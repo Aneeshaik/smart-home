a20c7d04a39bf0c397b1a33d2e96cd84cd720f5f
Anees <aneesnafeesimmu@gmail.com>
2024-08-22 23:32:17 +0530

    weather-added

--- src/components/Right.js
+++ src/components/Right.js
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 
 const Right = () => {
     const [time, setTime] = useState(new Date().toLocaleTimeString('en-us', {hour12: false}));
-    const date = new Date().toLocaleDateString('en-us', {month: 'long', day: '2-digit'});
+    const date = new Date().toLocaleDateString('en-us', {month: 'long', day: '2-digit', year: 'numeric'});
     const day = new Date().toLocaleDateString('en-us', {weekday: 'long'})
     const [weatherData, setWeatherData] = useState({});
+    const [loading, setLoading] = useState(true);
     const [hours, minutes] = time.split(':')
     useEffect(() => {
             const timer = setInterval(() => {
@@ -21,9 +22,10 @@ const Right = () => {
         console.log(apiKey); // Ensure this logs the correct API key
     
         try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`);
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}&units=metric`);
             const weatherData = await response.json();
             setWeatherData(weatherData);
+            setLoading(false);
             console.log(weatherData);
         } catch (error) {
             console.error('Error fetching weather data:', error);
@@ -32,13 +34,28 @@ const Right = () => {
     
     
 
-    return (
+    return loading ? null : (
+        <div>
         <div className="backdrop-blur-3xl m-2 bg-white/20 rounded-3xl text-white">
             <div className="p-3">
                 <h1 className="font-semibold text-2xl">{hours} : {minutes}</h1>
                 <h1>{day}, {date}</h1>
-                <h1>{weatherData.main.temp}</h1>
             </div>
+        </div>
+        <div className="backdrop-blur-3xl m-2 bg-white/20 rounded-3xl text-white">
+            <div className="text-left p-3 justify-between flex items-center">
+                <div className="flex flex-col items-left justify-between">
+                <h1 className="text-xl">Weather</h1>
+                <div className="flex items-baseline">
+                    <h1 className="text-4xl">{weatherData.main.temp}&deg;</h1>
+                    <h1 className="">{weatherData.weather[0].description}</h1>
+                </div>
+                </div>
+                <div className="items-center">
+                    <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="weather-icon" />
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
